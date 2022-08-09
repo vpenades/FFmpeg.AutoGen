52649d9cf46a10e24ff3e48bc739fff9e0aed239
vpenades <5433822+vpenades@users.noreply.github.com>
2022-08-09 09:16:45 +0200

    decorating a few structures

---
+++ FFmpeg.AutoGen/AVBufferRef.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFmpeg.AutoGen
+{
+    [System.Diagnostics.DebuggerDisplay("{size} bytes")]
+    partial struct AVBufferRef
+    {
+        public unsafe bool IsWriteable
+        {
+            get
+            {
+                var copy = this;
+                var result = ffmpeg.av_buffer_is_writable(&copy);
+                return result == 1;
+            }
+        }
+
+        public unsafe ReadOnlySpan<Byte> ReadOnlyBuffer
+        {
+            get
+            {
+                return new ReadOnlySpan<Byte>((void*)data, (int)size);
+            }
+        }
+
+        public unsafe Span<Byte> WriteableBuffer
+        {
+            get
+            {
+                if (!IsWriteable) throw new InvalidOperationException("The buffer is read only.");
+                return new Span<Byte>((void*)data, (int)size);
+            }
+        }
+    }
+}
---
+++ FFmpeg.AutoGen/AVRational.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFmpeg.AutoGen
+{
+    [System.Diagnostics.DebuggerDisplay("{num} / {den} = {Fraction}")]
+    partial struct AVRational : IComparable<AVRational>
+    {
+        /// <summary>
+        /// Convert an AVRational to a double.
+        /// </summary>
+        /// <remarks>
+        /// Equivalent to av_q2d
+        /// </remarks>
+        public double Fraction => num / (double)den;
+
+        /// <remarks>
+        /// return One of the following values:<br/>
+        /// - 0 if this == other<br/>
+        /// - 1 if this &lt; other<br/>
+        /// - -1 if this &gt; other<br/>
+        /// - INT_MIN if one of the values is of the form 0 / 0<br/>
+        /// Equivalent to av_cmp_q
+        /// </remarks>        
+        public int CompareTo(AVRational other)
+        {
+            Int64 tmp = this.num * (Int64)other.den - other.num * (Int64)this.den;
+
+            if (tmp != 0) return (int)((tmp ^ this.den ^ other.den) >> 63) | 1;
+            else if (other.den != 0 && this.den !=0) return 0;
+            else if (this.num != 0 && other.num != 0) return (this.num >> 31) - (other.num >> 31);
+            else return int.MinValue;
+        }
+    }
+}
